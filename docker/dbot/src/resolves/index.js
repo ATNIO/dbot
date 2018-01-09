525e97ef7e887d96ad713d5839ee3e6dbf9e451f
HuJingwei <a479540195@gmail.com>
2018-01-09 17:11:34 +0800

    ignore

--- docker/dbot/src/resolves/index.js
+++
@@ -1,9 +0,0 @@
-const uraidenResolve = require('./uraiden_resolve');
-const billResolve = require('./bill_resolve');
-const aiResolve = require('./ai_resolve');
-
-module.exports = {
-  uraidenResolve,
-  billResolve,
-  aiResolve
-}
--- docker/dbot/src/routes/index.js
+++
@@ -1,9 +0,0 @@
-const uraidenRouter = require('./uraiden_route');
-const billRouter = require('./bill_route');
-const aiRouter = require('./ai_route');
-
-module.exports = {
-  uraidenRouter,
-  billRouter,
-  aiRouter
-}
